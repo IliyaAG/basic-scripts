302c6bf74be88013feff7926bf4ffcfa12b1ddb1
IliyaAG <iliyaagh76@gmail.com>
2025-12-17 22:34:22 +0330

    Add colors

--- functions/print-coloric.sh
+++ functions/print-coloric.sh
@@ -10,6 +10,13 @@ case $color in
     red)    echo -e "\033[0;31mi -> $text\033[0m";;
     green)  echo -e "\033[0;32m -> $text\033[0m";;
     yellow) echo -e "\033[0;33m -> $text\033[0m";;
+    black)  echo -e " \033[0;30m -> $text\033[0m";;
+    blue)   echo -e "\033[0;34m -> $text\033[0m";;
+    magenta) echo -e "\033[0;35 -> $text\033[0m";;
+    cyan)   echo -e  "\033[0;36 -> $text\033[0m";;
+    wghite) echo -e  "\033[0;37 -> $text\033[0m";;
     *) echo -e "-> $text";;
 esac
+
+
 
