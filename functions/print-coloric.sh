4b5efa962743f073ce5150bafb9a90f11fa64cf7
IliyaAG <iliyaagh76@gmail.com>
2026-03-22 21:18:28 +0330

    Update print_coloric

--- functions/print-coloric.sh
+++ functions/print-coloric.sh
@@ -1,22 +1,17 @@
-#!/usr/bin/env bash
+print_coloric () {
+    local color=$1
+    shift
+    local text="$*"
 
-set -e
-
-local color=$1
-shift
-local text="$*"
-
-case $color in
-    red)    echo -e "\033[0;31mi -> $text\033[0m";;
-    green)  echo -e "\033[0;32m -> $text\033[0m";;
-    yellow) echo -e "\033[0;33m -> $text\033[0m";;
-    black)  echo -e " \033[0;30m -> $text\033[0m";;
-    blue)   echo -e "\033[0;34m -> $text\033[0m";;
-    magenta) echo -e "\033[0;35 -> $text\033[0m";;
-    cyan)   echo -e  "\033[0;36 -> $text\033[0m";;
-    wghite) echo -e  "\033[0;37 -> $text\033[0m";;
-    *) echo -e "-> $text";;
-esac
-
-
-
+    case $color in
+        red)    echo -e "\033[0;31m -> $text\033[0m";;
+        green)  echo -e "\033[0;32m -> $text\033[0m";;
+        yellow) echo -e "\033[0;33m -> $text\033[0m";;
+        black)  echo -e " \033[0;30m -> $text\033[0m";;
+        blue)   echo -e "\033[0;34m -> $text\033[0m";;
+        magenta) echo -e "\033[0;35m -> $text\033[0m";;
+        cyan)   echo -e  "\033[0;36m -> $text\033[0m";;
+        white) echo -e  "\033[0;37m -> $text\033[0m";;
+        *) echo -e "-> $text";;
+    esac
+}
