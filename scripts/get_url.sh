da421baa1a341e4d011d6e5d91b62accdc27a321
IliyaAG <iliyaagh76@gmail.com>
2026-04-02 22:53:35 +0330

    Update get_url

--- scripts/get_url.sh
+++ scripts/get_url.sh
@@ -6,8 +6,10 @@ PORT=80
 
 exec 3<>/dev/tcp/$HOST/$PORT
 
-#echo -e "GET $PATH"
-#
+echo -e "GET $PATH HTTP/1.1\r\nHost: $HOST\r\nConnection: close\r\n\r\n" >&3
+while RES= read -r line <&3; do
+    echo "$line"
+done
 
 exec 3>&-
 exec 3<&-
